41d437464ffc90ec529c1e995df9ae9797fb2f9d
Tan <gantuangco98@yahoo.com.ph>
2023-10-15 13:03:55 +0800

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Simple_Reflex_Agent
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.quadrant3 = new System.Windows.Forms.Label();
+            this.quadrant4 = new System.Windows.Forms.Label();
+            this.quadrant1 = new System.Windows.Forms.Label();
+            this.quadrant2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(893, 585);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // quadrant3
+            // 
+            this.quadrant3.AutoSize = true;
+            this.quadrant3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.quadrant3.Location = new System.Drawing.Point(160, 360);
+            this.quadrant3.Name = "quadrant3";
+            this.quadrant3.Size = new System.Drawing.Size(50, 13);
+            this.quadrant3.TabIndex = 1;
+            this.quadrant3.Text = "SAMPLE";
+            // 
+            // quadrant4
+            // 
+            this.quadrant4.AutoSize = true;
+            this.quadrant4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.quadrant4.Location = new System.Drawing.Point(537, 360);
+            this.quadrant4.Name = "quadrant4";
+            this.quadrant4.Size = new System.Drawing.Size(50, 13);
+            this.quadrant4.TabIndex = 2;
+            this.quadrant4.Text = "SAMPLE";
+            // 
+            // quadrant1
+            // 
+            this.quadrant1.AutoSize = true;
+            this.quadrant1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.quadrant1.Location = new System.Drawing.Point(537, 90);
+            this.quadrant1.Name = "quadrant1";
+            this.quadrant1.Size = new System.Drawing.Size(50, 13);
+            this.quadrant1.TabIndex = 3;
+            this.quadrant1.Text = "SAMPLE";
+            // 
+            // quadrant2
+            // 
+            this.quadrant2.AutoSize = true;
+            this.quadrant2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.quadrant2.Location = new System.Drawing.Point(160, 90);
+            this.quadrant2.Name = "quadrant2";
+            this.quadrant2.Size = new System.Drawing.Size(50, 13);
+            this.quadrant2.TabIndex = 4;
+            this.quadrant2.Text = "SAMPLE";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(893, 585);
+            this.Controls.Add(this.quadrant2);
+            this.Controls.Add(this.quadrant1);
+            this.Controls.Add(this.quadrant4);
+            this.Controls.Add(this.quadrant3);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Vacuum Cleaner";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label quadrant3;
+        private System.Windows.Forms.Label quadrant4;
+        private System.Windows.Forms.Label quadrant1;
+        private System.Windows.Forms.Label quadrant2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simple_Reflex_Agent
+{
+    public partial class Form1 : Form
+    {
+        private Random randomizer = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+            textLabels();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private int quadrant = new Random().Next(4);
+        private int prevQuadrant = 0;
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen pen = new Pen(Color.White);
+
+            int picBoxWidth = pictureBox1.Width;
+            int picBoxHeight = pictureBox1.Height;
+            int x = (picBoxWidth / 4) / 4;
+            int y = (picBoxHeight / 4) / 4;
+
+            int xSize = picBoxWidth - (picBoxWidth / 4);
+            int ySize = picBoxHeight - (picBoxHeight / 4);
+
+            int xCenter = x + xSize / 2;
+            int yCenter = y + ySize / 2;
+
+            g.DrawRectangle(pen, x, y, xSize, ySize);
+
+            g.DrawLine(pen, xCenter, y, xCenter, y + ySize);
+
+            g.DrawLine(pen, x, yCenter, x + xSize, yCenter);
+
+            Rectangle border = new Rectangle(x, y, xSize/2, ySize/2);
+            int newQuadrant = randomizer.Next(2);
+            switch (quadrant)
+            {
+                case 0:
+                    border.X = xCenter;
+                    prevQuadrant = quadrant;
+                    quadrant = (newQuadrant == 0) ? 1 : 3;  // randomizer transfer of the vacuum robot (UP, DOWN, LEFT, RIGHT)
+                    break;
+
+                case 1:
+                    prevQuadrant = quadrant;
+                    quadrant = (newQuadrant == 0) ? 0 : 2; // randomizer transfer of the vacuum robot (UP, DOWN, LEFT, RIGHT)
+                    break;
+
+                case 2:
+                    prevQuadrant = quadrant;
+                    border.Y = yCenter;
+                    quadrant = (newQuadrant == 0) ? 1 : 3; // randomizer transfer of the vacuum robot (UP, DOWN, LEFT, RIGHT)
+                    break;
+
+                case 3:
+                    prevQuadrant = quadrant;
+                    border.X = xCenter;
+                    border.Y = yCenter;
+                    quadrant = (newQuadrant == 0) ? 0 : 2; // randomizer transfer of the vacuum robot (UP, DOWN, LEFT, RIGHT)
+                    break;
+            }
+            g.DrawEllipse(pen, border);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+            check(prevQuadrant);
+        }
+
+        private void textLabels()
+        {
+            quadrant1.Text = (randomizer.Next(2) == 1) ? "CLEAN" : "DIRTY";
+            quadrant2.Text = (randomizer.Next(2) == 1) ? "CLEAN" : "DIRTY";
+            quadrant3.Text = (randomizer.Next(2) == 1) ? "CLEAN" : "DIRTY";
+            quadrant4.Text = (randomizer.Next(2) == 1) ? "CLEAN" : "DIRTY";
+        }
+
+        private void check(int quadrant)
+        {
+            switch (quadrant)
+            {
+                case 0:
+                    quadrant1.Text = "CHECKING";
+                    break;
+                case 1:
+                    quadrant2.Text = "CHECKING";
+                    break;
+                case 2:
+                    quadrant3.Text = "CHECKING";
+                    break;
+                case 3:
+                    quadrant4.Text = "CHECKING";
+                    break;
+            }
+        }
+    }
+}
